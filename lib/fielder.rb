8dac2c528d7dc9d73afddce3f5b8821a232a12ff
Janice Dunn & Brigham Thompson <student@epicodus.com>
2015-09-01 16:23:41 -0700

    add player and stats form pages

--- lib/fielder.rb
+++ lib/fielder.rb
@@ -1,3 +1,3 @@
 class Fielder < ActiveRecord::Base
-  belongs_to(:players)
+  belongs_to(:player)
 end
--- lib/kicker.rb
+++ lib/kicker.rb
@@ -1,3 +1,3 @@
 class Kicker < ActiveRecord::Base
-  belongs_to(:players)
+  belongs_to(:player)
 end
