0aa8708c1c8a605c2f9fe950d83dafb6f51f4900
Janice Dunn & Brigham Thompson <student@epicodus.com>
2015-09-03 13:45:23 -0700

    add the JD_Master changes to the master master

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,18 @@
 class Player < ActiveRecord::Base
   belongs_to(:team)
-  has_many(:kicker)
-  has_many(:pitcher)
-  has_many(:fielder)
+  has_many(:kickers)
+  has_many(:pitchers)
+  has_many(:fielders)
   validates(:name, {:presence => :true})
   before_save(:titleize_name)
+  before_save(:titleize_position)
 
   private
 
   define_method(:titleize_name) do
     self.name=(name().titleize())
+  end
+  define_method(:titleize_position) do
+    self.position=(position().titleize())
   end
 end
